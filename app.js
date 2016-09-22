5328d0cf5250601d57f598264b701c3ec105e020
Vishwas Gouru <gouru.vishwas123@gmail.com>
2016-09-22 12:02:24 -0400

    Done with the functionality

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 var app = angular.module("calculator", []);
 app.controller("CalculatorCtrl", CalculatorCtrl);
 
-function CalculationCtrl() {
+function CalculatorCtrl() {
+    
+    this.resultValue = 0;
+    this.buttonClicked = function(button) {
+        
+        this.selectedOption = button;
+        
+    }
+    
+    
+    this.showResult = function() {
+        
+        var number1 = parseFloat(this.input1);
+        var number2 = parseFloat(this.input2);
+        
+        
+        if(this.selectedOption === '+') {
+                this.resultValue = number1 + number2;
+            }
+        else if(this.selectedOption === '-') {
+                this.resultValue = number1 - number2;
+            }
+        else if(this.selectedOption === '*') {
+                this.resultValue = number1 * number2;
+            }
+        else if(this.selectedOption === '/') {
+                this.resultValue = number1 / number2;
+            }
+        
+        
+        
+        
+    }
     
 }
